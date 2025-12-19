142d9017e7f079d5bac118e07dc8542878c7d069
el-chris1 <727cdiazsolchaga@frhsd.com>
2025-12-19 10:43:35 -0500

    more fixes

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-function getPoints(value){
-    switch (value){
+function getPoints(value) {
+    switch (value) {
         case 'nice':
-            return 1
-            break;
-            case 'naughty':
+            return 1;
+        case 'naughty':
                 return -1;
-                break;
         default: 
-            return -1
+            return -1;
     }
 }
 
 function checkNaughtyOrNice () {
-    const q1 = document.getElementById['q1'].value
-    
-    const q2 = document.getElementById['q2'].value
-    
-    const q3 = document.getElementById['q3'].value
-    
-    const q4 = document.getElementById['q4'].value
-    
-    const q5 = document.getElementById['q5'].value
+    const q1 = document.getElementById('q1').value;
+    const q2 = document.getElementById('q2').value;
+    const q3 = document.getElementById('q3').value;
+    const q4 = document.getElementById('q4').value;
+    const q5 = document.getElementById('q5').value;
 
-}
+
     const score = getPoints(q1) + getPoints(q2) + getPoints(q3) + getPoints(q4) + getPoints(q5)
 
     if (score > 2.5) {
-        alert('ho ho ho! on the nice list you go!')
-    } else{
-        alert('slow your role, here is some coal!')
+        alert("ho ho ho! on the nice list you go!");
+        console.log(score);
+    } else {
+        alert("slow your role, here is some coal!");
+        console.log(score);
     }
+}
